5a91054d5681c269a3f29b84adddb97224cf0490
John Fletcher <john.a.fletcher@seagate.com>
2022-03-01 14:11:42 -0700

    UDX-7178 Helm updates

--- deploy_pilot_link.sh
+++ deploy_pilot_link.sh
@@ -77,7 +77,6 @@ done
 set -- "${POSITIONAL_ARGS[@]}"
 
 namespace=$NAMESPACE
-clusterrolebinding=$namespace"-pilot-link-ctrlr-rbac-binding"
 name="pilot-link-ctrlr"
 
 if [[ $namespace == "" ]]
@@ -115,21 +114,11 @@ fi
 
 if [[ $TYPE == "detect" ]]
 then
-    pv_detection_name=$namespace"-pilot-link-ctrlr-detection"
-    pv_detection_hostpath="/mnt/pilot-link/storage/"$namespace
-    pv_hostdevices_name=$namespace"-pilot-link-ctrlr-hostdevices"
-    pv_hostsys_name=$namespace"-pilot-link-ctrlr-hostsys"
-    
     printf "Creating $namespace-$name\n"
     helm install $namespace"-"$name $BASE_PATH/helm_pkg/pilot-link-ctrlr \
-        --set pilotlinkctrlr.clusterrolebinding.name=$clusterrolebinding \
         --set namespace=$namespace \
         --set pilotlinkctrlr.pod.image=$PILOT_LINK_CTRLR_IMAGE \
         --set pilotlinkctrlr.nodeselector.pilotlinknodelabel=$pilotlinknodelabel \
-        --set pilotlinkctrlr.pv.detection.name=$pv_detection_name \
-        --set pilotlinkctrlr.pv.detection.hostpath=$pv_detection_hostpath \
-        --set pilotlinkctrlr.pv.hostdevices.name=$pv_hostdevices_name \
-        --set pilotlinkctrlr.pv.hostsys.name=$pv_hostsys_name \
         --set pilotlinkctrlr.dataservices.type=$TYPE \
         --set pilotlinkctrlr.dataservices.image=$PILOT_LINK_DS_IMAGE \
         --set pilotlinkctrlr.secret.lppassword=$LP_PASSWORD \
@@ -137,7 +126,6 @@ then
 else
     printf "Creating $namespace-$name\n"
     helm install $namespace"-"$name $BASE_PATH/helm_pkg/pilot-link-ctrlr \
-        --set pilotlinkctrlr.clusterrolebinding.name=$clusterrolebinding \
         --set namespace=$namespace \
         --set pilotlinkctrlr.pod.image=$PILOT_LINK_CTRLR_IMAGE \
         --set pilotlinkctrlr.dataservices.type=$TYPE \
